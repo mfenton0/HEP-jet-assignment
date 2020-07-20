164de5cf1884d72d2e9609c2c0c7db9debf9a59c
davidho27941 <jimsam3456@cycu.org.tw>
2020-07-20 22:05:34 +0800

    fix: typo

--- analysis_script/parse.py
+++ analysis_script/parse.py
@@ -17,12 +17,17 @@ class parse_function():
                             "Particle.M2", "Particle.D1", "Particle.D2","Particle.PID",
                             "Particle.PT","Particle.Eta","Particle.Phi",
                             "Particle.Mass"], start=0, stop=2000, step=None) # in this example I set
+        self.jet = root2array(self.file, "Delphes;1", branches=["Jet.PT", "Jet.Eta", "Jet.Phi","Jet.Mass"])
         self._Status, self._M1, self._M2, self._D1, self._D2, self._PID, self._PT, self._Eta, self._Phi, self._Mass = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 
         self.Labels = ["Status", "M1", "M2", "D1", "D2", "PID", "PT", "Eta", "Phi", "Mass"]
         
     def parse_event(self):
 
         return self.event
+
+    def parse_jet(self):
+        
+        return self.jet
 
     def count_particles(self):           
 
@@ -36,11 +41,16 @@ class parse_function():
 
     def save_parsed_file(self):
 
-        with open(self.path, "w") as f:
-            f.write("Index\tstatus\tM1\tM2\tD1\tD2\tPID\tPT\tETA\tPHI\tMASS")
+        FILENAME = ""
+        seq = ("parsed_", time_tag, ".txt")
+
+        store_path = os.join.path(self.path, FILENAME.join(seq))
+
+        with open(store_path, "w") as f:
+            f.write("Index,status,M1,M2,D1,D2,PID,PT,ETA,PHI,MASS")
             f.write("\n")
             for j in range(len(self.event[0][0])):
-                f.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7:.3f}\t{8:.3f}\t{9:.3f}\t{10:.3f}".format( 
+                f.write("{0},{1},{2},{3},{4},{5},{6},{7:.3f},{8:.3f},{9:.3f},{10:.3f}".format( 
                         j, 
                         self.event[0][self._Status][j], 
                         self.event[0][self._M1][j],
@@ -59,11 +69,15 @@ def main():
 
     root_file = sys.argv[1]  #your root file path
     time_tag = time.strftime("%Y_%m_%d_%H_%M", time.localtime())
-    FILENAME = ""
-    seq = ("parsed_", time_tag, ".txt")
-    PATH = os.path.join(os.getcwd(), FILENAME.join(seq))
+    
+    PATH = os.getcwd(), FILENAME.join(seq)
     parsed_event = parse_function(root_file, PATH)
+    event_log = parsed_event.parse_event()
+    jet_log = parsed_event.parse_jet()
     particle_numbers = parsed_event.count_particles()
+    
+    #print(jet_log)
+
     saved_path = parsed_event.save_parsed_file()
     print("The data has been parsed! There are {0} particles in this events.\nThe result has been store in '{1}'.".format(particle_numbers, saved_path))
 
