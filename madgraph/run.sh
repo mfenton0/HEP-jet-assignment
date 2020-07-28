4a1c58e151b84bb13f37e9f1b22791ca1a38cc54
davidho27941 <jimsam3456@cycu.org.tw>
2020-07-28 12:40:25 +0800

    feat: add new ignore rule

---
+++ madgraph/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+nohup ./bin/mg5_aMC pptt.txt
+nohup ./bin/mg5_aMC pptt.txt
+nohup ./bin/mg5_aMC pptt.txt
+nohup ./bin/mg5_aMC pptt.txt
+nohup ./bin/mg5_aMC pptt.txt
+nohup ./bin/mg5_aMC pptt.txt
+nohup ./bin/mg5_aMC pptt.txt
+nohup ./bin/mg5_aMC pptt.txt
+nohup ./bin/mg5_aMC pptt.txt
+nohup ./bin/mg5_aMC pptt.txt
