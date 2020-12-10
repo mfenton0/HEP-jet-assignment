b7b436905eec707237fd021d7a3f28f3ed6ad8ac
davidho27941 <jimsam3456@cycu.org.tw>
2020-12-10 14:49:01 +0000

    feat: add lines for 1 to 1 parsing.

--- madgraph/four_top_preparation/run_pptttt.sh
+++ madgraph/four_top_preparation/run_pptttt.sh
@@ -5,12 +5,18 @@ for i in {1..9};do
        	then
 		echo $i
         	nohup ./bin/mg5_aMC pptttt_first_round.txt >> /home/david/log.txt
+            nohup python3 /home/david/workplace/HEP-jet-assignment/analysis_script/main.py -u parse -m four_top -i /home/david/pptttt/Events/run_0$i/tag_1_delphes_events.root -o event_record_four_top_$i.h5 -s 1 
+            rm /home/david/pptttt/Events/run_0$i/*
 	else
  		echo $i
 		nohup ./bin/mg5_aMC pptttt.txt >> /home/david/log.txt
+        nohup python3 /home/david/workplace/HEP-jet-assignment/analysis_script/main.py -u parse -m four_top -i /home/david/pptttt/Events/run_0$i/tag_1_delphes_events.root -o event_record_four_top_$i.h5 -s 1 
+        rm /home/david/pptttt/Events/run_0$i/*
 	fi
 done
 for i in {10..11};do
 	echo $i
        	nohup ./bin/mg5_aMC pptttt.txt >> /home/david/log.txt
+        nohup python3 /home/david/workplace/HEP-jet-assignment/analysis_script/main.py -u parse -m four_top -i /home/david/pptttt/Events/run_$i/tag_1_delphes_events.root -o event_record_four_top_$i.h5 -s 1 
+        rm /home/david/pptttt/Events/run_$i/*
 done
