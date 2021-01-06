8ca79036777d1561eaa683c4b9bf6846509a3b42
davidho27941 <jimsam3456@cycu.org.tw>
2021-01-06 07:13:46 +0000

    feat: add env variable to make script more easy to be modified.

--- madgraph/ttbar-semi-lep_preparation/run_pptt_lep.sh
+++ madgraph/ttbar-semi-lep_preparation/run_pptt_lep.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 source ~/.bash_profile
+ANALYSIS_SCRIPT_PATH='/home/david/workplace/HEP-jet-assignment/analysis_script'
+ROOT_FILE_PATH='/home/david/pptt_lep/Events'
+LOG_FILE_PATH='/home/david'
+echo "Path of script: $ANALYSIS_SCRIPT_PATH"
+echo "path of root file: $ROOT_FILE_PATH"
+echo "path of log file: $LOG_FILE_PATH"
 for i in {1..9};do
 	if [ $i == 1 ]
        	then
 		echo $i 
-        	nohup ./bin/mg5_aMC pptt_lep_first_round.txt >> /home/david/log_lep.txt 
-		nohup python3 /home/david/workplace/HEP-jet-assignment/analysis_script/main.py -u parse -m ttbar -i /home/david/pptt_lep/Events/run_0$i/tag_1_delphes_events.root -o event_record_top_lep_$i.h5 -s 1 >> /home/david/log_lep.txt
-		rm /home/david/pptt_lep/Events/run_0$i/*
+        	nohup ./bin/mg5_aMC pptt_lep_first_round.txt >> $LOG_FILE_PATH/log_lep.txt 
+		nohup python3 $ANALYSIS_SCRIPT_PATH/main.py -u parse -m ttbar -i $ROOT_FILE_PATH/run_0$i/tag_1_delphes_events.root -o event_record_top_lep_$i.h5 -s 1 >> $LOG_FILE_PATH/log_lep.txt
+		rm $ROOT_FILE_PATH/run_0$i/*
 	else
  		echo $i
-		nohup ./bin/mg5_aMC pptt_lep.txt >> /home/david/log_lep.txt
-		nohup python3 /home/david/workplace/HEP-jet-assignment/analysis_script/main.py -u parse -m ttbar -i /home/david/pptt_lep/Events/run_0$i/tag_1_delphes_events.root -o event_record_top_lep_$i.h5 -s 1 >> /home/david/log_lep.txt
-		rm /home/david/pptt_lep/Events/run_0$i/*
+		nohup ./bin/mg5_aMC pptt_lep.txt >> $LOG_FILE_PATH/log_lep.txt
+		nohup python3 $ANALYSIS_SCRIPT_PATH/main.py -u parse -m ttbar -i $ROOT_FILE_PATH/run_0$i/tag_1_delphes_events.root -o event_record_top_lep_$i.h5 -s 1 >> $LOG_FILE_PATH/log_lep.txt
+		rm $ROOT_FILE_PATH/run_0$i/*
 	fi 		
 done
 for i in {10..11};do
 	echo $i
-       	nohup ./bin/mg5_aMC pptt_lep.txt >> /home/david/log_lep.txt
-	nohup python3 /home/david/workplace/HEP-jet-assignment/analysis_script/main.py -u parse -m ttbar -i /home/david/pptt_lep/Events/run_$i/tag_1_delphes_events.root -o event_record_top_lep_$i.h5 -s 1 >> /home/david/log_lep.txt
-	rm /home/david/pptt_lep/Events/run_$i/*
+       	nohup ./bin/mg5_aMC pptt_lep.txt >> $LOG_FILE_PATH/log_lep.txt
+	nohup python3 $ANALYSIS_SCRIPT_PATH/main.py -u parse -m ttbar -i $ROOT_FILE_PATH/run_$i/tag_1_delphes_events.root -o event_record_top_lep_$i.h5 -s 1 >> $LOG_FILE_PATH/log_lep.txt
+	rm $ROOT_FILE_PATH/run_$i/*
 done
