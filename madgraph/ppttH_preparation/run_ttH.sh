f4ecae79cba274c2cdf4d538626413691d713389
davidho27941 <jimsam3456@cycu.org.tw>
2021-01-06 06:16:17 +0000

    feat: adapt to new version analysis script.

--- madgraph/ppttH_preparation/run_ttH.sh
+++ madgraph/ppttH_preparation/run_ttH.sh
@@ -4,19 +4,19 @@ for i in {1..9};do
 	if [ $i == 1 ]
        	then
 		echo $i 
-        	nohup ./bin/mg5_aMC ttH_first_run.txt >> /home/david/log_ttH.txt 
-		nohup python3 parse_uproot_release_ttH.py /home/david/ppttH/Events/run_0$i/tag_1_delphes_events.root /home/david/mass_generation/event_record_0$i.h5 >> /home/david/log.txt
+        	nohup ./bin/mg5_aMC ttH_first_round.txt >> /home/david/log_ttH.txt 
+		nohup python3 /home/david/workplace/HEP-jet-assignment/analysis_script/main.py -u parse -m ttH -i /home/david/ppttH/Events/run_0$i/tag_1_delphes_events.root -o event_record_ttH_$i.h5 -s 1
 		rm /home/david/ppttH/Events/run_0$i/*
 	else
  		echo $i
 		nohup ./bin/mg5_aMC ttH.txt >> /home/david/log_ttH.txt
-		nohup python3 parse_uproot_release_ttH.py /home/david/pptt/Events/run_0$i/tag_1_delphes_events.root /home/david/mass_generation/event_record_0$i.h5 >> /home/david/log.txt
+		nohup python3 /home/david/workplace/HEP-jet-assignment/analysis_script/main.py -u parse -m ttH -i /home/david/ppttH/Events/run_0$i/tag_1_delphes_events.root -o event_record_ttH_$i.h5 -s 1
 		rm /home/david/ppttH/Events/run_0$i/*
 	fi 		
 done
 for i in {10..100};do
 	echo $i
 	nohup ./bin/mg5_aMC ttH.txt >> /home/david/log_ttH.txt
-	nohup python3 parse_uproot_release_ttH.py /home/david/pptt/Events/run_$i/tag_1_delphes_events.root /home/david/mass_generation/event_record_$i.h5 >> /home/david/log.txt
+	nohup python3 /home/david/workplace/HEP-jet-assignment/analysis_script/main.py -u parse -m ttH -i /home/david/ppttH/Events/run_$i/tag_1_delphes_events.root -o event_record_ttH_$i.h5 -s 1
 	rm /home/david/ppttH/Events/run_$i/*
 done
