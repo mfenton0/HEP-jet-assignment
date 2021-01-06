a75840f882c13389f3bdda12a20d6d9abc0d26ad
davidho27941 <jimsam3456@cycu.org.tw>
2021-01-06 07:01:33 +0000

    feat: add command for auto-analysis workflow.

--- madgraph/ttbar-semi-lep_preparation/run_pptt_lep.sh
+++ madgraph/ttbar-semi-lep_preparation/run_pptt_lep.sh
@@ -5,18 +5,18 @@ for i in {1..9};do
        	then
 		echo $i 
         	nohup ./bin/mg5_aMC pptt_lep_first_round.txt >> /home/david/log_lep.txt 
-		#nohup python3 parse_uproot_release.py /home/david/pptt_lep/Events/run_0$i/tag_1_delphes_events.root /home/david/mass_generation/event_record_0$i.h5 >> /home/david/log_lep.txt
-		#rm /home/david/pptt_lep/Events/run_0$i/*
+		nohup python3 /home/david/workplace/HEP-jet-assignment/analysis_script/main.py -u parse -m ttbar -i /home/david/pptt_lep/Events/run_0$i/tag_1_delphes_events.root -o event_record_top_lep_$i.h5 -s 1 >> /home/david/log_lep.txt
+		rm /home/david/pptt_lep/Events/run_0$i/*
 	else
  		echo $i
 		nohup ./bin/mg5_aMC pptt_lep.txt >> /home/david/log_lep.txt
-		#nohup python3 parse_uproot_release.py /home/david/pptt_lep/Events/run_0$i/tag_1_delphes_events.root /home/david/mass_generation/event_record_0$i.h5 >> /home/david/log_lep.txt
-		#rm /home/david/pptt_lep/Events/run_0$i/*
+		nohup python3 /home/david/workplace/HEP-jet-assignment/analysis_script/main.py -u parse -m ttbar -i /home/david/pptt_lep/Events/run_0$i/tag_1_delphes_events.root -o event_record_top_lep_$i.h5 -s 1 >> /home/david/log_lep.txt
+		rm /home/david/pptt_lep/Events/run_0$i/*
 	fi 		
 done
 for i in {10..11};do
 	echo $i
        	nohup ./bin/mg5_aMC pptt_lep.txt >> /home/david/log_lep.txt
-        #nohup python3 parse_uproot_release.py /home/david/pptt_lep/Events/run_$i/tag_1_delphes_events.root /home/david/mass_generation/event_record_$i.h5 >> /home/david/log_lep.txt
-	#rm /home/david/pptt_lep/Events/run_$i/*
+	nohup python3 /home/david/workplace/HEP-jet-assignment/analysis_script/main.py -u parse -m ttbar -i /home/david/pptt_lep/Events/run_$i/tag_1_delphes_events.root -o event_record_top_lep_$i.h5 -s 1 >> /home/david/log_lep.txt
+	rm /home/david/pptt_lep/Events/run_$i/*
 done
