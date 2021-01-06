a5b6bba43b781e74af3d021c7c473eaa83592147
davidho27941 <jimsam3456@cycu.org.tw>
2021-01-06 07:00:29 +0000

    fix: fix typo

--- madgraph/pptt_preparation/run.sh
+++ madgraph/pptt_preparation/run.sh
@@ -5,18 +5,18 @@ for i in {1..9};do
        	then
 		echo $i 
         	nohup ./bin/mg5_aMC pptt_first_round.txt >> /home/david/log.txt 
-		nohup python3 /home/david/workplace/HEP-jet-assignment/analysis_script/main.py -u parse -m four_top -i /home/david/pptt/Events/run_0$i/tag_1_delphes_events.root -o event_record_top_FHD_$i.h5 -s 1 >> /home/david/log.txt
-		#rm /home/david/pptt/Events/run_0$i/*
+		nohup python3 /home/david/workplace/HEP-jet-assignment/analysis_script/main.py -u parse -m ttbar -i /home/david/pptt/Events/run_0$i/tag_1_delphes_events.root -o event_record_top_FHD_$i.h5 -s 1 >> /home/david/log.txt
+		rm /home/david/pptt/Events/run_0$i/*
 	else
  		echo $i
 		nohup ./bin/mg5_aMC pptt.txt >> /home/david/log.txt
-		nohup python3 /home/david/workplace/HEP-jet-assignment/analysis_script/main.py -u parse -m four_top -i /home/david/pptt/Events/run_0$i/tag_1_delphes_events.root -o event_record_top_FHD_$i.h5 -s 1 >> /home/david/log.txt
-		#rm /home/david/pptt/Events/run_0$i/*
+		nohup python3 /home/david/workplace/HEP-jet-assignment/analysis_script/main.py -u parse -m ttbar -i /home/david/pptt/Events/run_0$i/tag_1_delphes_events.root -o event_record_top_FHD_$i.h5 -s 1 >> /home/david/log.txt
+		rm /home/david/pptt/Events/run_0$i/*
 	fi 		
 done
 for i in {10..11};do
 	echo $i
        	nohup ./bin/mg5_aMC pptt.txt >> /home/david/log.txt
-	nohup python3 /home/david/workplace/HEP-jet-assignment/analysis_script/main.py -u parse -m four_top -i /home/david/pptt/Events/run_$i/tag_1_delphes_events.root -o event_record_top_FHD_$i.h5 -s 1 >> /home/david/log.txt
-	#rm /home/david/pptt/Events/run_$i/*
+	nohup python3 /home/david/workplace/HEP-jet-assignment/analysis_script/main.py -u parse -m ttbar -i /home/david/pptt/Events/run_$i/tag_1_delphes_events.root -o event_record_top_FHD_$i.h5 -s 1 >> /home/david/log.txt
+	rm /home/david/pptt/Events/run_$i/*
 done
