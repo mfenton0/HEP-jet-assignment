f2bf0967721669f4fdf3f077b0c416d05b7cdf17
davidho27941 <jimsam3456@cycu.org.tw>
2021-03-17 02:53:54 +0000

    feat: add docker script for generate cr_qluon sample with multi-container

---
+++ docker_script/pptt/ttbar_cr_gluon/main_command.sh
@@ -0,0 +1,7 @@
+docker run \
+    -it \
+    --cpus="1.0" \
+    -v /home/alex/Research/FeynmanData/vagrant1:/home/david \
+    -v /home/alex/Research/FeynmanData/workplace:/home/workplace \
+    alan200276/centos:SVJsimulation \
+    /bin/bash -c /home/workplace/HEP-jet-assignment/docker_script/pptt/ttbar_cr_gluon/ttbar_cr_gluon.sh    
