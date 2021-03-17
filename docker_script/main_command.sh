302a145ba6534b1c6ae4a60194c6b638fbb4891e
davidho27941 <jimsam3456@cycu.org.tw>
2021-03-17 02:25:25 +0000

    feat: delete moved files.

--- docker_script/main_command.sh
+++
@@ -1,8 +0,0 @@
-docker run \
-    -it \
-    --cpus="1.0" \
-    -v /home/alex/Research/FeynmanData/vagrant1:/home/david \
-    -v /home/alex/Research/FeynmanData/workplace:/home/workplace \
-    alan200276/centos:SVJsimulation \
-    /bin/bash -c /home/workplace/HEP-jet-assignment/docker_script/ttbar.sh
-    
