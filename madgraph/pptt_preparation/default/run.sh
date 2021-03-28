21f3ba0b1625fa5e0d14302a1eb562ba66786d77
davidho27941 <jimsam3456@cycu.org.tw>
2021-03-28 15:03:16 +0000

    feat: remove multi-job option of herwig.

--- madgraph/pptt_preparation/default/run.sh
+++ madgraph/pptt_preparation/default/run.sh
@@ -23,7 +23,7 @@ for i in {1..9};do
 		sed -E -i "s/run_[0-9]{2,}/run_0${i}/g" pptt_mg_herwig.in 
 		#grep 'run_' pptt_mg_herwig.in
 		Herwig read pptt_mg_herwig.in >> $LOG_FILE_PATH/log.txt
-		Herwig run pptt_mg_herwig.run -j 1 -N 10000 -s $SEED -d 1 >> $LOG_FILE_PATH/log.txt
+		Herwig run pptt_mg_herwig.run -N 10000 -s $SEED -d 1 >> $LOG_FILE_PATH/log.txt
 		/delphes/DelphesHepMC /MG5_aMC_v2_7_3/Delphes/cards/delphes_card_ATLAS.tcl $ROOT_FILE_PATH/run_0$i/herwig_run_0$i.root  $ROOT_FILE_PATH/run_0$i/pptt_angular_run_0$i.hepmc
 		python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -m ttbar -i $ROOT_FILE_PATH/run_0$i/tag_1_delphes_events.root -o /mass_generation/event_record_top_FHD_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
 		python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -g herwig7 -m ttbar -i $ROOT_FILE_PATH/run_0$i/herwig_run_0$i.root -o /mass_generation/event_record_top_FHD_herwig_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
@@ -37,7 +37,7 @@ for i in {1..9};do
 		sed -E -i "s/run_[0-9]{2,}/run_0${i}/g" pptt_mg_herwig.in
 		#grep 'run_' pptt_mg_herwig.in
 		Herwig read pptt_mg_herwig.in >> $LOG_FILE_PATH/log.txt
-		Herwig run pptt_mg_herwig.run -j 1 -N 10000 -s $(( $SEED + $i )) -d 1 >> $LOG_FILE_PATH/log.txt
+		Herwig run pptt_mg_herwig.run -N 10000 -s $(( $SEED + $i )) -d 1 >> $LOG_FILE_PATH/log.txt
 		/delphes/DelphesHepMC /MG5_aMC_v2_7_3/Delphes/cards/delphes_card_ATLAS.tcl $ROOT_FILE_PATH/run_0$i/herwig_run_0$i.root  $ROOT_FILE_PATH/run_0$i/pptt_angular_run_0$i.hepmc
 		python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -m ttbar -i $ROOT_FILE_PATH/run_0$i/tag_1_delphes_events.root -o /mass_generation/event_record_top_FHD_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
 		python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -g herwig7 -m ttbar -i $ROOT_FILE_PATH/run_0$i/herwig_run_0$i.root -o /mass_generation/event_record_top_FHD_herwig_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
@@ -53,9 +53,8 @@ for i in {10..100};do
 	#grep 'run_' pptt_mg_herwig.in
         Herwig read pptt_mg_herwig.in >> $LOG_FILE_PATH/log.txt
         Herwig run pptt_mg_herwig.run -N 10000 -s $(( $SEED + $i )) -d 1 >> $LOG_FILE_PATH/log.txt
-	/delphes/DelphesHepMC /MG5_aMC_v2_7_3/Delphes/cards/delphes_card_ATLAS.tcl run_0$i.root  $ROOT_FILE_PATH/run_$i/pptt_angular_run_0$i.hepmc
+	/delphes/DelphesHepMC /MG5_aMC_v2_7_3/Delphes/cards/delphes_card_ATLAS.tcl run_$i.root  $ROOT_FILE_PATH/run_$i/pptt_angular_run_$i.hepmc
 	python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -m ttbar -i $ROOT_FILE_PATH/run_$i/tag_1_delphes_events.root -o /mass_generation/event_record_top_FHD_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
-	python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -g herwig7 -m ttbar -i $ROOT_FILE_PATH/run_$i/herwig_run_0$i.root -o /mass_generation/event_record_top_FHD_herwig_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
+	python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -g herwig7 -m ttbar -i $ROOT_FILE_PATH/run_$i/herwig_run_$i.root -o /mass_generation/event_record_top_FHD_herwig_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
 	#rm $ROOT_FILE_PATH/run_$i/*
 done
-
