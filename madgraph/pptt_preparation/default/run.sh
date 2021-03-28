88c12074011b437b69ba0bda4175f915d4e837a8
davidho27941 <jimsam3456@cycu.org.tw>
2021-03-28 15:17:34 +0000

    feat: change the file configuration for mg+hw7 generation.

--- madgraph/pptt_preparation/default/run.sh
+++ madgraph/pptt_preparation/default/run.sh
@@ -9,8 +9,8 @@ export OMP_NUM_THREADS=1
 mkdir -p /mass_generation
 
 ANALYSIS_SCRIPT_PATH='/workplace/HEP-jet-assignment/analysis_script'
-ROOT_FILE_PATH='/MAP_VOLUME_FOR_CONTAINER/pptt/Events'
-LOG_FILE_PATH='/MAP_VOLUME_FOR_CONTAINER'
+ROOT_FILE_PATH='/home/david/pptt/Events'
+LOG_FILE_PATH='/home/david'
 echo "Path of script: $ANALYSIS_SCRIPT_PATH"
 echo "path of root file: $ROOT_FILE_PATH"
 echo "path of log file: $LOG_FILE_PATH"
@@ -25,9 +25,9 @@ for i in {1..9};do
 		Herwig read pptt_mg_herwig.in >> $LOG_FILE_PATH/log.txt
 		Herwig run pptt_mg_herwig.run -N 10000 -s $SEED -d 1 >> $LOG_FILE_PATH/log.txt
 		/delphes/DelphesHepMC /MG5_aMC_v2_7_3/Delphes/cards/delphes_card_ATLAS.tcl $ROOT_FILE_PATH/run_0$i/herwig_run_0$i.root  $ROOT_FILE_PATH/run_0$i/pptt_angular_run_0$i.hepmc
-		python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -m ttbar -i $ROOT_FILE_PATH/run_0$i/tag_1_delphes_events.root -o /mass_generation/event_record_top_FHD_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
-		python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -g herwig7 -m ttbar -i $ROOT_FILE_PATH/run_0$i/herwig_run_0$i.root -o /mass_generation/event_record_top_FHD_herwig_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
-		#rm $ROOT_FILE_PATH/run_0$i/*
+		python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -m ttbar -i $ROOT_FILE_PATH/run_0$i/tag_1_delphes_events.root -o /home/david/mass_generation/event_record_top_FHD_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
+		python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -g herwig7 -m ttbar -i $ROOT_FILE_PATH/run_0$i/herwig_run_0$i.root -o /home/david/mass_generation/event_record_top_FHD_herwig_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
+		rm $ROOT_FILE_PATH/run_0$i/*
 	else
  		echo $i
 		#echo "seed = " $(( $SEED + $i ))
@@ -39,9 +39,9 @@ for i in {1..9};do
 		Herwig read pptt_mg_herwig.in >> $LOG_FILE_PATH/log.txt
 		Herwig run pptt_mg_herwig.run -N 10000 -s $(( $SEED + $i )) -d 1 >> $LOG_FILE_PATH/log.txt
 		/delphes/DelphesHepMC /MG5_aMC_v2_7_3/Delphes/cards/delphes_card_ATLAS.tcl $ROOT_FILE_PATH/run_0$i/herwig_run_0$i.root  $ROOT_FILE_PATH/run_0$i/pptt_angular_run_0$i.hepmc
-		python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -m ttbar -i $ROOT_FILE_PATH/run_0$i/tag_1_delphes_events.root -o /mass_generation/event_record_top_FHD_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
-		python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -g herwig7 -m ttbar -i $ROOT_FILE_PATH/run_0$i/herwig_run_0$i.root -o /mass_generation/event_record_top_FHD_herwig_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
-		#rm $ROOT_FILE_PATH/run_0$i/*
+		python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -m ttbar -i $ROOT_FILE_PATH/run_0$i/tag_1_delphes_events.root -o /home/david/mass_generation/event_record_top_FHD_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
+		python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -g herwig7 -m ttbar -i $ROOT_FILE_PATH/run_0$i/herwig_run_0$i.root -o /home/david/mass_generation/event_record_top_FHD_herwig_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
+		rm $ROOT_FILE_PATH/run_0$i/*
 	fi 		
 done
 for i in {10..100};do
@@ -54,7 +54,7 @@ for i in {10..100};do
         Herwig read pptt_mg_herwig.in >> $LOG_FILE_PATH/log.txt
         Herwig run pptt_mg_herwig.run -N 10000 -s $(( $SEED + $i )) -d 1 >> $LOG_FILE_PATH/log.txt
 	/delphes/DelphesHepMC /MG5_aMC_v2_7_3/Delphes/cards/delphes_card_ATLAS.tcl run_$i.root  $ROOT_FILE_PATH/run_$i/pptt_angular_run_$i.hepmc
-	python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -m ttbar -i $ROOT_FILE_PATH/run_$i/tag_1_delphes_events.root -o /mass_generation/event_record_top_FHD_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
-	python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -g herwig7 -m ttbar -i $ROOT_FILE_PATH/run_$i/herwig_run_$i.root -o /mass_generation/event_record_top_FHD_herwig_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
-	#rm $ROOT_FILE_PATH/run_$i/*
+	python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -m ttbar -i $ROOT_FILE_PATH/run_$i/tag_1_delphes_events.root -o /home/david/mass_generation/event_record_top_FHD_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
+	python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -g herwig7 -m ttbar -i $ROOT_FILE_PATH/run_$i/herwig_run_$i.root -o /home/david/mass_generation/event_record_top_FHD_herwig_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
+	rm $ROOT_FILE_PATH/run_$i/*
 done
