0116e68aad4da9614c2496034c94abd18b927d41
davidho27941 <jimsam3456@cycu.org.tw>
2021-03-28 15:17:08 +0000

    feat: change the file configuration for mg+hw7 generation.

--- docker_script/pptt/ttbar_FHD/ttbar.sh
+++ docker_script/pptt/ttbar_FHD/ttbar.sh
@@ -1,11 +1,13 @@
 # Set up environment
-source ~/.bash_profile;
-cd ~/MG5_aMC_v2_7_2;
+source ~/.bashrc
+source /ROOT/bin/thisroot.sh
+source /herwig/bin/activate
+cd /MG5_aMC_v2_7_2;
 pip3 install h5py tqdm;
 
 # Copy over all of the necessary files to run the script
-/bin/cp -f /home/workplace/HEP-jet-assignment/madgraph/delphes_card_ATLAS.tcl ~/MG5_aMC_v2_7_2/Delphes/cards/;
-/bin/cp -avr /home/workplace/HEP-jet-assignment/madgraph/pptt_preparation/default/* ./;
+/bin/cp -f /workplace/HEP-jet-assignment/madgraph/delphes_card_ATLAS.tcl /MG5_aMC_v2_7_2/Delphes/cards/;
+/bin/cp -avr /workplace/HEP-jet-assignment/madgraph/pptt_preparation/default/* ./;
 
 # Set Seed
 SEED=$(cat /home/david/seed.txt)
