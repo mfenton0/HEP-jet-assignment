521a9abffbfe9976bbe0b735011c6f39fe8d50eb
davidho27941 <jimsam3456@cycu.org.tw>
2021-03-30 05:37:31 +0000

    feat: correct the path of MG5

--- docker_script/pptt/ttbar_FHD/ttbar.sh
+++ docker_script/pptt/ttbar_FHD/ttbar.sh
@@ -2,15 +2,15 @@
 source ~/.bashrc
 source /ROOT/bin/thisroot.sh
 source /herwig/bin/activate
-cd /MG5_aMC_v2_7_2;
+cd /MG5_aMC_v2_7_3;
 pip3 install h5py tqdm;
 
 # Copy over all of the necessary files to run the script
-/bin/cp -f /workplace/HEP-jet-assignment/madgraph/delphes_card_ATLAS.tcl /MG5_aMC_v2_7_2/Delphes/cards/;
+/bin/cp -f /workplace/HEP-jet-assignment/madgraph/delphes_card_ATLAS.tcl /MG5_aMC_v2_7_3/Delphes/cards/;
 /bin/cp -avr /workplace/HEP-jet-assignment/madgraph/pptt_preparation/default/* ./;
 
 # Set Seed
-SEED=$(cat /home/david/seed.txt)
+export SEED=$(cat /home/david/seed.txt)
 awk -v seedval=$SEED -F"=" 'BEGIN{OFS=FS} $1=="set iseed "{$2=" "seedval}1' pptt.txt > pptt2.txt && mv -f pptt2.txt pptt.txt
 awk -v seedval=$SEED -F"=" 'BEGIN{OFS=FS} $1=="set iseed "{$2=" "seedval}1' pptt_first_round.txt > pptt_first_round2.txt && mv -f pptt_first_round2.txt pptt_first_round.txt
 
