d121b884519fbeb9545b0375960dde8e275dec84
davidho27941 <jimsam3456@cycu.org.tw>
2021-05-18 13:51:35 +0000

    feat: update command for new version analysis

--- madgraph/ppttH_preparation/run_ttH.sh
+++ madgraph/ppttH_preparation/run_ttH.sh
@@ -1,22 +1,47 @@
 #!/bin/bash
-source ~/.bash_profile
-for i in {1..9};do
+source ~/.bashrc
+source /ROOT/bin/thisroot.sh
+source /herwig/bin/activate
+export MKL_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+
+mkdir -p /home/david/mass_generation
+
+ANALYSIS_SCRIPT_PATH='/workplace/HEP-jet-assignment/analysis_script'
+ROOT_FILE_PATH='/home/david/ppttH/Events'
+LOG_FILE_PATH='/home/david'
+echo "Path of script: $ANALYSIS_SCRIPT_PATH"
+echo "path of root file: $ROOT_FILE_PATH"
+echo "path of log file: $LOG_FILE_PATH"
+for i in {1..1};do
 	if [ $i == 1 ]
        	then
-		echo $i 
-        	nohup ./bin/mg5_aMC ttH_first_round.txt >> /home/david/log_ttH.txt 
-		nohup python3 /home/david/workplace/HEP-jet-assignment/analysis_script/main.py -u parse -m ttH -i /home/david/ppttH/Events/run_0$i/tag_1_delphes_events.root -o event_record_ttH_$i.h5 -s 1
-		rm /home/david/ppttH/Events/run_0$i/*
+		echo "Running round: $i, SEED: $SEED"
+		echo "Running MG5."
+	        ./bin/mg5_aMC ttH_first_round.txt >> $LOG_FILE_PATH/log.txt
+		echo "Parsing root file generate by pythia8 showering."
+		python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -m ttH -i $ROOT_FILE_PATH/run_0$i/tag_1_delphes_events.root -o /home/david/mass_generation/event_record_ttH_FHD_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
+		echo "Removing generate files."
+		rm $ROOT_FILE_PATH/run_0$i/*
 	else
- 		echo $i
-		nohup ./bin/mg5_aMC ttH.txt >> /home/david/log_ttH.txt
-		nohup python3 /home/david/workplace/HEP-jet-assignment/analysis_script/main.py -u parse -m ttH -i /home/david/ppttH/Events/run_0$i/tag_1_delphes_events.root -o event_record_ttH_$i.h5 -s 1
-		rm /home/david/ppttH/Events/run_0$i/*
+ 		echo "Running round: $i, SEED: $SEED"
+		awk -F"=" 'BEGIN{OFS=FS} $1=="set iseed "{$2=" "$2+1}1' ttH.txt > ttH2.txt && mv -f ttH2.txt ttH.txt
+		echo "Running MG5."
+		./bin/mg5_aMC ttH.txt >> $LOG_FILE_PATH/log.txt
+		echo "Parsing root file generate by pythia8 showering."
+		python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -m ttH -i $ROOT_FILE_PATH/run_0$i/tag_1_delphes_events.root -o /home/david/mass_generation/event_record_ttH_FHD_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
+		echo "Removing generate files."
+		rm $ROOT_FILE_PATH/run_0$i/*
 	fi 		
 done
-for i in {10..100};do
-	echo $i
-	nohup ./bin/mg5_aMC ttH.txt >> /home/david/log_ttH.txt
-	nohup python3 /home/david/workplace/HEP-jet-assignment/analysis_script/main.py -u parse -m ttH -i /home/david/ppttH/Events/run_$i/tag_1_delphes_events.root -o event_record_ttH_$i.h5 -s 1
-	rm /home/david/ppttH/Events/run_$i/*
-done
+#for i in {10..100};do
+# 	echo "Running round: $i, SEED: $SEED"
+#	awk -F"=" 'BEGIN{OFS=FS} $1=="set iseed "{$2=" "$2+1}1' ttH.txt > ttH2.txt && mv -f pptt2.txt ttH.txt
+#	echo "Running MG5."
+#	./bin/mg5_aMC ttH.txt >> $LOG_FILE_PATH/log.txt
+#	echo "Parsing root file generate by pythia8 showering."
+#	python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -m ttH -i $ROOT_FILE_PATH/run_$i/tag_1_delphes_events.root -o /home/david/mass_generation/event_record_ttH_FHD_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
+#	echo "Removing generate files."
+#	rm $ROOT_FILE_PATH/run_$i/*
+#done
