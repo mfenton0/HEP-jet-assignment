c2df58484b1a20edf4f8817ec717b62404e66ab8
davidho27941 <jimsam3456@cycu.org.tw>
2021-05-25 23:15:53 +0000

    feat: comment some lines.

--- madgraph/four_top_preparation/run_pptttt.sh
+++ madgraph/four_top_preparation/run_pptttt.sh
@@ -14,7 +14,7 @@ LOG_FILE_PATH='/home/david'
 echo "Path of script: $ANALYSIS_SCRIPT_PATH"
 echo "path of root file: $ROOT_FILE_PATH"
 echo "path of log file: $LOG_FILE_PATH"
-for i in {1..9};do
+for i in {1..1};do
 	if [ $i == 1 ]
        	then
 		echo "Running round: $i, SEED: $SEED"
@@ -35,13 +35,13 @@ for i in {1..9};do
 		rm $ROOT_FILE_PATH/run_0$i/*
 	fi 		
 done
-for i in {10..100};do
- 	echo "Running round: $i, SEED: $SEED"
-	awk -F"=" 'BEGIN{OFS=FS} $1=="set iseed "{$2=" "$2+1}1' pptttt.txt > pptttt2.txt && mv -f pptttt2.txt pptttt.txt
-	echo "Running MG5."
-	./bin/mg5_aMC pptttt.txt >> $LOG_FILE_PATH/log.txt
-	echo "Parsing root file generate by pythia8 showering."
-	python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -m four_top -i $ROOT_FILE_PATH/run_$i/tag_1_delphes_events.root -o /home/david/mass_generation/event_record_four_top_FHD_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
-	echo "Removing generate files."
-	rm $ROOT_FILE_PATH/run_$i/*
-done
+#for i in {10..100};do
+# 	echo "Running round: $i, SEED: $SEED"
+#	awk -F"=" 'BEGIN{OFS=FS} $1=="set iseed "{$2=" "$2+1}1' pptttt.txt > pptttt2.txt && mv -f pptttt2.txt pptttt.txt
+#	echo "Running MG5."
+#	./bin/mg5_aMC pptttt.txt >> $LOG_FILE_PATH/log.txt
+#	echo "Parsing root file generate by pythia8 showering."
+#	python3 $ANALYSIS_SCRIPT_PATH/main.py -p 1 -u parse -m four_top -i $ROOT_FILE_PATH/run_$i/tag_1_delphes_events.root -o /home/david/mass_generation/event_record_four_top_FHD_$i.npz -s 1 >> $LOG_FILE_PATH/log.txt
+#	echo "Removing generate files."
+#	rm $ROOT_FILE_PATH/run_$i/*
+#done
