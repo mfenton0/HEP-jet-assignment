ca4d6ad954388d69a74bee70bacd3a70bc49b13a
davidho27941 <jimsam3456@cycu.org.tw>
2021-06-20 15:00:14 +0000

    feat: add model description in -m flag.

--- analysis_script/main.py
+++ analysis_script/main.py
@@ -15,7 +15,7 @@ def main():
     parser.add_argument("-u", "--usage", dest="usage", help="Define the purpose to run.")
     parser.add_argument("-i", "--input-file", dest="input",  help="Input file directory.")
     parser.add_argument("-o", "--output-file", dest="output",  help="Output file directory.")
-    parser.add_argument("-m", "--model", dest="model", help="Select a model to parse. Usable mode: ttbar, ttH, four_top, bkg")
+    parser.add_argument("-m", "--model", dest="model", help="Select a model to parse. Usable mode: ttbar, ttH, four_top, bkg, ")
     parser.add_argument("-c", "--config", dest="config", help="The directory configuration file for cutflow.")
     parser.add_argument("-s", "--single", dest="single", default=True, help="Determin is dealing with single file or not. If not dealing with single file, please input the directory of root files.")
     parser.add_argument("-p", "--num_process", dest="process", default=1, type=int, help="Number of extra process for accelerating speed.")
