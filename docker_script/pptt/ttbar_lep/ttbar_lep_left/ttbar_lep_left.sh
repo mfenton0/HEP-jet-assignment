c9baaa63043cf0486345d662deb493494ac2f0fe
davidho27941 <jimsam3456@cycu.org.tw>
2021-06-21 22:37:58 +0800

    feat: add multi container configuration for semi-leptonic model

---
+++ docker_script/pptt/ttbar_lep/ttbar_lep_left/ttbar_lep_left.sh
@@ -0,0 +1,20 @@
+# Set up environment
+source ~/.bash_profile;
+cd ~/MG5_aMC_v2_7_2;
+pip3 install h5py tqdm;
+
+# Copy over all of the necessary files to run the script
+/bin/cp -f /workplace/HEP-jet-assignment/madgraph/delphes_card_ATLAS.tcl ~/MG5_aMC_v2_7_2/Delphes/cards/;
+/bin/cp -avr /workplace/HEP-jet-assignment/madgraph/ttbar-semi-lep_preparation/left/* ./;
+
+# Set Seed
+SEED=$(cat /home/david/seed.txt)
+awk -v seedval=$SEED -F"=" 'BEGIN{OFS=FS} $1=="set iseed "{$2=" "seedval}1' pptt_lep.txt > pptt_lep2.txt && mv -f pptt_lep2.txt pptt_lep.txt
+awk -v seedval=$SEED -F"=" 'BEGIN{OFS=FS} $1=="set iseed "{$2=" "seedval}1' pptt_lep_first_round.txt > pptt_lep_first_round2.txt && mv -f pptt_lep_first_round2.txt pptt_lep_first_round.txt
+
+# Create output directory and log our config
+mkdir -p /home/david/mass_generation_lep;
+/bin/cp pptt.txt /home/david/mass_generation_lep/
+
+# Start the simulation
+./run.sh
---
+++ docker_script/pptt/ttbar_lep/ttbar_lep_right/create.sh
@@ -0,0 +1,26 @@
+if [ $# -lt 3 ]; then
+	echo "Usage: create.sh INITIAL_SEED NUM_RUNS NUM_PARALLEL"
+	exit 1
+fi
+
+INITIALSEED="$1"
+NUM_RUNS="$2"
+PARALLEL="$3"
+
+HOME=`pwd`
+
+for (( i=0; i<$PARALLEL; i++ ))
+do
+	j=`expr $NUM_RUNS \* $i + $INITIALSEED + 1`
+	echo $j
+
+	mkdir docker_$j
+	echo $j > docker_$j/seed.txt
+
+	docker run \
+		-d \
+		-v $HOME/workplace:/workplace \
+		-v $HOME/docker_$j:/home/david \
+		alan200276/centos:SVJsimulation \
+		/bin/bash -c /workplace/HEP-jet-assignment/docker_script/pptt/ttbar_lep_right/ttbar_lep_right.sh
+done
